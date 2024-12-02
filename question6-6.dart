45368906e063cc762c8528971867e4b40b444ec2
unknown <worametpee16@gmail.com>
2024-12-02 16:11:59 +0700

    ข้อ 46ะ

---
+++ question6-6.dart
@@ -0,0 +1,29 @@
+abstract class Bottle{
+  factory Bottle(String brand){
+    if(brand == 'Papsi'){
+      return PapsiBottle();
+    }
+   return CokeBottle();
+  }
+ open();
+}
+class CokeBottle implements Bottle{
+  @override
+  open() {
+    print(super.toString());
+    print('Cokebottle is opened');
+  }
+}
+
+class PapsiBottle implements Bottle{
+  @override
+  open() {
+    print(super.toString());
+    print('Papsibottle is opened');
+  }
+}
+
+void main(){
+  Bottle coke =Bottle('Fanta');
+  coke.open();
+}
